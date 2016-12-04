2fa11b505119a38c12d3d683c7081f58164f8042
Diego Marques <dmarquesdev@gmail.com>
2016-12-04 14:22:19 -0200

    Install script support for arch and debian/ubuntu based distros

--- projeto2/install.sh
+++ projeto2/install.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 echo "[*] Checking Node.js installation..."
 VERSION=$(node -v)
+ERROR=false
 
 if [[ -z "$VERSION" ]]; then
   echo "[-] Seems like Node.js isn't installed! Installing..."
-  apt-get update && apt-get install -y nodejs npm nodejs-legacy
+  DISTRO=$(cat /etc/*elease | grep -e ^ID= | cut -d = -f 2 | tr '[:upper:]' '[:lower:]')
+  if [ "$DISTRO" -eq "ubuntu" ] || [ "$DISTRO" -eq "debian" ]; then
+    curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+    apt-get install -y nodejs
+  elif [ "$DISTRO" -eq "arch" ]; then
+    pacman -S nodejs npm
+  else
+    ERROR=true
+    echo "[-] This script only supports Arch Linux and Debian based distros! \
+    Try to install nodejs and npm manually and run 'npm start'"
+  fi
 fi
 
-echo "[*] Installing dependencies via NPM..."
-npm install
-echo "[*] All done! use 'npm start' to start the application!"
+if [[ "$ERROR" -eq false ]] then;
+  echo "[*] Installing dependencies via NPM..."
+  npm install
+  echo "[*] All done! use 'npm start' to start the application!"
+fi
