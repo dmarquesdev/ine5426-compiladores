b207539832a96194ee5cf773e26d8d41357d0c1a
Diego Marques <dmarquesdev@gmail.com>
2016-12-04 17:08:17 -0200

    Context update installer

--- projeto2/install.sh
+++ projeto2/install.sh
@@ -5,10 +5,12 @@ VERSION=$(node -v)
 if [[ -z "$VERSION" ]]; then
   echo "[-] Seems like Node.js isn't installed! Installing..."
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
   nvm install node
 fi
 
 echo "[*] Installing dependencies via NPM..."
-npm install
 npm install -g webpack electron
+npm install
 echo "[*] All done! use 'npm start' to start the application!"
