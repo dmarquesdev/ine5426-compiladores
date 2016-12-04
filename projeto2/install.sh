7aa69011a7b8bd5e2d43ab190e4805e9a75ad798
Diego Marques <dmarquesdev@gmail.com>
2016-12-04 16:40:45 -0200

    Installer using nvm

--- projeto2/install.sh
+++ projeto2/install.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 echo "[*] Checking Node.js installation..."
 VERSION=$(node -v)
-ERROR=false
 
 if [[ -z "$VERSION" ]]; then
   echo "[-] Seems like Node.js isn't installed! Installing..."
-  DISTRO=$(cat /etc/*elease | grep -e ^ID= | cut -d = -f 2 | tr '[:upper:]' '[:lower:]')
-  if [[ "$DISTRO" -eq "ubuntu" ]] || [[ "$DISTRO" -eq "debian" ]]; then
-    curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-    apt-get install -y nodejs
-  elif [ "$DISTRO" -eq "arch" ]; then
-    pacman -S nodejs npm
-  else
-    ERROR=true
-    echo "[-] This script only supports Arch Linux and Debian based distros! Try to install nodejs and npm manually and run 'npm install' to install project dependencies"
-  fi
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+  nvm install node
 fi
 
-if [[ "$ERROR" -eq false ]]; then
-  echo "[*] Installing dependencies via NPM..."
-  npm install
-  npm install -g webpack electron
-  echo "[*] All done! use 'npm start' to start the application!"
-fi
+echo "[*] Installing dependencies via NPM..."
+npm install
+npm install -g webpack electron
+echo "[*] All done! use 'npm start' to start the application!"
